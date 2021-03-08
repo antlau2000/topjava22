5f0d5eec71ec381d9b7ec8d8eb27fa6561bc2c40
antlau2000 <61512568+antlau2000@users.noreply.github.com>
2021-03-08 13:16:01 +0300

    HW3 optional - add tests on somebody else's meal

--- src/test/java/ru/javawebinar/topjava/service/MealServiceTest.java
+++ src/test/java/ru/javawebinar/topjava/service/MealServiceTest.java
@@ -3,6 +3,7 @@ package ru.javawebinar.topjava.service;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.jdbc.SqlConfig;
@@ -11,11 +12,13 @@ import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.util.exception.NotFoundException;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.Month;
 import java.util.List;
 
 import static org.junit.Assert.assertThrows;
 import static ru.javawebinar.topjava.MealTestData.*;
+import static ru.javawebinar.topjava.UserTestData.ADMIN_ID;
 import static ru.javawebinar.topjava.UserTestData.USER_ID;
 import static ru.javawebinar.topjava.UtilAssert.assertMatch;
 
@@ -75,5 +78,17 @@ public class MealServiceTest {
     @Test
     public void getSomebodyElseMeal() {
         assertThrows(NotFoundException.class, () -> service.get(MEAL_1_ADMIN, USER_ID));
+    }
+
+    @Test
+    public void updateSomebodyElseMeal() {
+        assertThrows(NotFoundException.class, () ->
+                service.update(new Meal(MEAL_1_USER, LocalDateTime.now(), "update", 99), ADMIN_ID));
+    }
+
+    @Test
+    public void deleteSomebodyElseMeal() {
+        assertThrows(NotFoundException.class, () ->
+                service.delete(MEAL_1_USER, ADMIN_ID));
     }
 }
